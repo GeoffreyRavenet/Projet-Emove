eee9c301377efd79f26b0a9280dba055b2e51e90
GeoffreyRavenet <ravenet.geoffreypro@gmail.com>
2018-07-05 16:45:10 +0200

    Add file + button delete
    Ajoute de l'enregistrement de formulaire + ajout d'image + un bouton pour supprimer le vehicule

--- LocationDeVoiture/app.js
+++ LocationDeVoiture/app.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var nunjucks = require('nunjucks');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+
+var upload = multer({
+    dest: __dirname + '/uploads'
+});
 
 mongoose.connect('mongodb://localhost/LocationDeVoiture');
 
@@ -9,11 +15,15 @@ require('./models/Marque');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(upload.single('file'));
 
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 app.use('/', require('./routes/vehicules'));
 app.use('/marques', require('./routes/marques'));
+
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 nunjucks.configure('views' , {
         autoescape: true,
--- LocationDeVoiture/routes/vehicules.js
+++ LocationDeVoiture/routes/vehicules.js
@@ -11,14 +11,14 @@ router.get('/',(req, res) => {
 router.get('/new', (req, res) => {
     Marque.find({}).then(marques => {
         var vehicule = new Vehicule();
-        res.render('vehicules/edit.html', {vehicule: vehicule , marques: marques});
+        res.render('vehicules/edit.html', {vehicule: vehicule , marques: marques, endpoint: '/'});
     })
    
 })
 router.get('/edit/:id', (req , res) => {
     Marque.find({}).then(marques =>{
         Vehicule.findById(req.params.id).then(vehicule =>{
-            res.render('vehicules/edit.html', {vehicule: vehicule, marques: marques}); 
+            res.render('vehicules/edit.html', {vehicule: vehicule, marques: marques ,endpoint: '/' + vehicule._id.toString()}); 
         }),
         err => res.status(500).send(err);
     })
@@ -29,5 +29,26 @@ router.get('/:id' , (req, res) => {
         res.render('vehicules/show.html', {vehicule: vehicule});  
     }),
     err => res.status(500).send(err);
+})
+
+router.post('/:id?', (req, res) =>{
+    new Promise((resolve , reject) =>{
+        if(req.params.id){
+            Vehicule.findById(req.params.id).then(resolve , reject)
+        }else{
+            resolve(new Vehicule())
+        }
+    }).then(vehicule =>{
+        vehicule.name = req.body.name;
+        vehicule.description = req.body.description;
+        vehicule.number = req.body.number;
+        vehicule.marques = req.body.marques;
+
+        if(req.file) vehicule.picture = req.file.filename;
+
+        return vehicule.save();
+    }).then(()=>{
+        res.redirect('/');
+    })
 })
 module.exports = router;
